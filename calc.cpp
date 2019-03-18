4fa7c413dc01ca351728c3adcb78cf360694d171
vibhav2000 <vibhav.sharma2000@gmail.com>
2019-03-18 13:15:15 +0530

    added default case

--- calc.cpp
+++ calc.cpp
@@ -28,7 +28,10 @@ cout << "Enter two operands: ";
         case '/':
             cout << num1/num2;
             break;
-        
+default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;        
             }
 
     return 0;
