2e7fd7d2ce54195c6c724089924255ecfa9629cb
vibhav2000 <vibhav.sharma2000@gmail.com>
2019-03-18 13:10:46 +0530

    using swith case,operators add and sub

--- calc.cpp
+++ calc.cpp
@@ -1,84 +1,23 @@
 #include <iostream>
-
 using namespace std;
 
-#include <stdlib.h>
-
-int main ()
-
+int main()
 {
-    double D1, D2 , D3 (0);
-    char	Operator;
-    const bool T(true), F(false);
+	char op;
+	float num1, num2;
 
-    do
-    {
-        cout <<"\nEnter the first number:";
-        cin>>D1;
-        cout << "\nPlease enter an operator (+ - * / or type X to exit or C to restart): "<< endl;
-        cin >> Operator;
-    } while (Operator == 'C');
+cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
 
-    while(!F)
+cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
     {
-        switch (Operator)
-        {
-        case '+' :
-            cout<<"Enter second number : ";
-            cin>>D2;
-            D3=D1+D2;
-            cout<< D1<< " + " << D2 << " = " << D3 << endl;
-            T;
+        case '+':
+            cout << num1+num2;
             break;
 
         case '-':
-            cout<<"Enter second number : ";
-            cin>>D2;
-            D3=D1-D2;
-            cout<< D1<< " - " << D1 << " = " << D3 << endl;
-            T;
-            break;
-
-        case '*':
-            cout<<"Enter second number : ";
-            cin>>D2;
-            D3=D1*D2;
-            cout<< D1<< " * " << D2 << " = " << D3 << endl;
-
-            T;
-            break;
-
-        case'/':
-            cout<<"Enter second number : ";
-            cin>>D2;
-            if ( D2==0)
-            {
-                cout << "Undefined answer(s). Enter new divisor.." << endl;
-                cin >> D2;
-            }
-            else D3= D1/D2;
-            cout << D1 << " / " << D2 << " = " << D3 << endl;
-            T;
-            break;
-
-        case 'C':
-        case 'c':
-            D1=0;
-            cout << " Enter first number ";
-            cin>>D1;
-            T;
-            break;
-        case 'X' :
-        case 'x' :
-            T;
-            exit (0) ;
-            break;
-        default:
-            F;
-            cout << "Invalid response " << Operator << " - Please enter a valid operation - Enter X to quit or enter C to clear" << endl;
-
-        }
-        D1=D3;
-        cout << "Enter new operator" << endl;
-    }
-}
+            cout << num1-num2;
+            break;	
